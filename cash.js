562a88142a85e50695471abe9321b0f9cdc19945
deluwarhoisn <babuhossen301@gmail.com>
2025-08-02 00:23:49 +0600

    trim and toLowerCase

--- cash.js
+++ cash.js
@@ -1,11 +1,22 @@
 const school = 'Raj Uk Uttara Model Scholl';
-// console.log(school);
+console.log(school);
+console.log(school.toUpperCase());
+// console.log(school.toLowerCase());
 
 const subject = 'Chemistry';
-const book = 'Chemistry';
-if(subject === book){
+const book = 'chemIstry';
+if(subject.toLowerCase === book.toLowerCase){
     console.log('i am reading book aibar porikkhai fatay falbo')
 }
 else{
     console.log('hudai hudai prista ultai ar vat khabona ')
+}
+const drink = '  water';
+const liquid = '  water'
+
+if(drink.trim() === liquid.trim()){
+    console.log('panir opre name jibon')
+}
+else{
+    console.log('somudrer pani khaite parena lobon akto')
 }
