afd5f4be40cadaa3d8e9110e6e44165328269cee
deluwarhoisn <babuhossen301@gmail.com>
2025-08-02 00:04:04 +0600

    cash and if else

---
+++ cash.js
@@ -0,0 +1,11 @@
+const school = 'Raj Uk Uttara Model Scholl';
+// console.log(school);
+
+const subject = 'Chemistry';
+const book = 'Chemistry';
+if(subject === book){
+    console.log('i am reading book aibar porikkhai fatay falbo')
+}
+else{
+    console.log('hudai hudai prista ultai ar vat khabona ')
+}
