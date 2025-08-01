5cd18fad229873632fc939d735fc080c0363e836
deluwarhoisn <babuhossen301@gmail.com>
2025-08-01 22:14:36 +0600

    string is imutable

--- string.js
+++ string.js
@@ -1,3 +1,21 @@
 const country = 'bangladesh';
-const division = "Newkhali";
-const district = ``
+const division = 'true';
+const district = `B-Bariya`
+const thana = new String ('Demra');
+
+// console.log(typeof division);
+console.log(typeof thana);
+const numbers = [54, 98, 78, 21, 65];
+console.log(numbers);
+numbers[1] = 11;
+console.log(numbers)
+// string is imutable --> not changeable
+
+// console.log(numbers.length);
+// console.log(numbers[1]);
+const capital = 'Dhaka'
+console.log(capital);
+capital[2] = 'o';
+console.log(capital);
+// console.log(capital.length);
+// console.log(capital[2]);
